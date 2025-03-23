8af87350061b9e65f0fc8af9c408a11088f41118
jithu-keyvalue <jithu@keyvalue.systems>
2025-03-23 17:06:59 +0530

    update problem

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ users = {}
 @app.post("/api/users")
 def create_user(data):  # 📝 TODO: Add type annotation to read request body
     # 📝 TODO: Save the user data with auto-generated id
-    return {"message": "User profile saved!"}
+    return {"id": "user_id", "message": "User profile saved!"}
 
 @app.get("/api/users/{id}")
 def get_user(id): # 📝 TODO: Add type annotation to read request body
